c5b95592712166729e1473209089507a09e7748e
Vladimir Gavrilov <gavriloff.v@yandex.ru>
2021-03-31 23:40:07 +0300

    Серые страны

--- js/init.js
+++ js/init.js
@@ -1,83 +1,67 @@
-$(function(){
-	
-	let r = Raphael('map', 1050, 1050),
+$(function () {
+
+	var r = Raphael('map', 1050, 1050),
 		attributes = {
-            fill: '#fff',
-            stroke: '#3899E6',
-            'stroke-width': 1,
-            'stroke-linejoin': 'round'
-        },
-		attributes1 = {
-			fill: '#red',
+			fill: '#ccc',
 			stroke: '#3899E6',
 			'stroke-width': 1,
 			'stroke-linejoin': 'round'
 		},
 		arr = new Array();
-	
-	for (let country in paths) {
-		
-		let obj = r.path(paths[country].path);
-		
-		if (paths[country].gruppa == 0)
-		{ 
-			obj.attr(attributes1);		
-		} else { 
-			obj.attr(attributes);		
-		}
-		
-		
+
+	for (var country in paths) {
+
+		var obj = r.path(paths[country].path);
+
+		obj.attr(attributes);
+			
 		arr[obj.id] = country;
-		
-		obj
-		.hover(function(){
+//  событие hover
+		obj.hover(function () {
 			this.animate({
 				fill: '#1669AD'
 			}, 300);
-		}, function(){
+		}, function () {
 			this.animate({
 				fill: attributes.fill
 			}, 300);
-		})
-		.click(function(){
-			document.location.hash = arr[this.id];
-			
-			let point = this.getBBox(0);
-			
-			$('#map').next('.point').remove();
-			
-			$('#map').after($('<div />').addClass('point'));
-			
-			$('.point')
-			.html(paths[arr[this.id]].name)
-			.prepend($('<a />').attr('href', '#').addClass('close').text('Закрыть'))
-			.prepend($('<img />').attr('src', 'flags/'+arr[this.id]+'.png'))
-			.css({
-				left: point.x+(point.width/2)-80,
-				top: point.y+(point.height/2)-20
-			})
-			.fadeIn();
-			
 		});
-		
-		$('.point').find('.close').live('click', function(){
-			let t = $(this),
+		obj.click(function () {
+			document.location.hash = arr[this.id];
+			// меняем адрес документа (после #)
+			var point = this.getBBox(0);
+			// возвращает размер элемента
+			$('#map').next('.point').remove();
+			$('#map').after($('<div />').addClass('point'));
+			// удаляем существующий div с классом point и создаём ещё один
+			$('.point')
+				.html(paths[arr[this.id]].name)
+				.prepend($('<a />').attr('href', '#').addClass('close').text('Close'))
+				.prepend($('<img />').attr('src', 'flags/' + arr[this.id] + '.png'))
+				.css({
+					left: point.x + (point.width / 2) - 80,
+					top: point.y + (point.height / 2) - 20
+				})
+				.fadeIn();
+			// добавляем контент (название страны, рисунок и кнопку закрытия),
+			// задаём позицию и показваем элемент
+		});
+//обработчик для кнопки «закрыть»
+		$('.point').find('.close').live('click', function () {
+			var t = $(this),
 				parent = t.parent('.point');
-			
-			parent.fadeOut(function(){
+			parent.fadeOut(function () {
 				parent.remove();
 			});
 			return false;
 		});
-		
-		
-		 
-		
-	}
-		
-			
-});
-let voteMay = 1e6;
-let getVolForm = document.getElementById("paragrafForm");;
 
-getVolForm.innerHTML = " Введите число от 1 до "+ voteMay;
+
+
+
+	}
+
+
+});
+
+getVolForm.innerHTML = 'Введите число от 1 до '+ voteMay;
