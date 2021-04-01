7f273f8f2893846131be159b382d4dcd57bd240b
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-01 09:11:23 +0300

    Страна отобрал

--- js/init.js
+++ js/init.js
@@ -11,7 +11,7 @@ $(function () {
 	arrCountPlayer = ['portugal', 'spain', 'belgium', 'italy', 'belarus', 'poland', 'finland', 'germany', 'sweden', 'turkey', 'gb', 'switzerland', 'austria', 'czech', 'slovakia', 'nungary', 'france', 'denmark', 'russia'];
 
 	for (var country in paths) {
-		for (let country of arrCountPlayer) {
+		if (arrCountPlayer.indexOf(country) === -1) {
 			
 		var obj = r.path(paths[country].path);
 
@@ -21,43 +21,91 @@ $(function () {
 //  событие hover
 		obj.hover(function () {
 			this.animate({
-				fill: '#1669AD'
+				fill: '#ffa07a'
 			}, 300);
 		}, function () {
 			this.animate({
 				fill: attributes.fill
 			}, 300);
 		});
-		obj.click(function () {
-			document.location.hash = arr[this.id];
-			// меняем адрес документа (после #)
-			var point = this.getBBox(0);
-			// возвращает размер элемента
-			$('#map').next('.point').remove();
-			$('#map').after($('<div />').addClass('point'));
-			// удаляем существующий div с классом point и создаём ещё один
-			$('.point')
-				.html(paths[arr[this.id]].name)
-				.prepend($('<a />').attr('href', '#').addClass('close').text('Close'))
-				.prepend($('<img />').attr('src', 'flags/' + arr[this.id] + '.png'))
-				.css({
-					left: point.x + (point.width / 2) - 80,
-					top: point.y + (point.height / 2) - 20
-				})
-				.fadeIn();
-			// добавляем контент (название страны, рисунок и кнопку закрытия),
-			// задаём позицию и показваем элемент
-		});
-//обработчик для кнопки «закрыть»
-		$('.point').find('.close').live('click', function () {
-			var t = $(this),
-				parent = t.parent('.point');
-			parent.fadeOut(function () {
-				parent.remove();
-			});
-			return false;
-		});
+// 		obj.click(function () {
+// 			document.location.hash = arr[this.id];
+// 			// меняем адрес документа (после #)
+// 			var point = this.getBBox(0);
+// 			// возвращает размер элемента
+// 			$('#map').next('.point').remove();
+// 			$('#map').after($('<div />').addClass('point'));
+// 			// удаляем существующий div с классом point и создаём ещё один
+// 			$('.point')
+// 				.html(paths[arr[this.id]].name)
+// 				.prepend($('<a />').attr('href', '#').addClass('close').text('Close'))
+// 				.prepend($('<img />').attr('src', 'flags/' + arr[this.id] + '.png'))
+// 				.css({
+// 					left: point.x + (point.width / 2) - 80,
+// 					top: point.y + (point.height / 2) - 20
+// 				})
+// 				.fadeIn();
+// 			// добавляем контент (название страны, рисунок и кнопку закрытия),
+// 			// задаём позицию и показваем элемент
+// 		});
+// //обработчик для кнопки «закрыть»
+// 		$('.point').find('.close').live('click', function () {
+// 			var t = $(this),
+// 				parent = t.parent('.point');
+// 			parent.fadeOut(function () {
+// 				parent.remove();
+// 			});
+// 			return false;
+// 		});
 
+
+
+
+	}else{
+			var obj = r.path(paths[country].path);
+
+			obj.attr(attributes);
+
+			arr[obj.id] = country;
+			//  событие hover
+			obj.hover(function () {
+				this.animate({
+					fill: '#1669AD'
+				}, 300);
+			}, function () {
+				this.animate({
+					fill: attributes.fill
+				}, 300);
+			});
+			obj.click(function () {
+				document.location.hash = arr[this.id];
+				// меняем адрес документа (после #)
+				var point = this.getBBox(0);
+				// возвращает размер элемента
+				$('#map').next('.point').remove();
+				$('#map').after($('<div />').addClass('point'));
+				// удаляем существующий div с классом point и создаём ещё один
+				$('.point')
+					.html(paths[arr[this.id]].name)
+					.prepend($('<a />').attr('href', '#').addClass('close').text('Close'))
+					.prepend($('<img />').attr('src', 'flags/' + arr[this.id] + '.png'))
+					.css({
+						left: point.x + (point.width / 2) - 80,
+						top: point.y + (point.height / 2) - 20
+					})
+					.fadeIn();
+				// добавляем контент (название страны, рисунок и кнопку закрытия),
+				// задаём позицию и показваем элемент
+			});
+			//обработчик для кнопки «закрыть»
+			$('.point').find('.close').live('click', function () {
+				var t = $(this),
+					parent = t.parent('.point');
+				parent.fadeOut(function () {
+					parent.remove();
+				});
+				return false;
+			});
 
 
 
